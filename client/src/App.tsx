ba70948dcd05db0e77a44549d6512b3d6fd5132c
Andy Tran <transient.tran@gmail.com>
2020-05-30 21:20:46 -0400

    display canvas and memory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Canvas from './components/Canvas';
+import Canvas from "./components/Canvas";
+import Memory from "./components/Memory";
 
 function App() {
-  return (
-    <div className="App">
-        <h4>I'm making a game!</h4>
+    return (
+        <div className="App">
+            <h4>I'm making a game!</h4>
 
-      <Canvas />
-    </div>
-  );
+            <Canvas />
+
+            <hr />
+
+            <Memory />
+        </div>
+    );
 }
 
 export default App;
