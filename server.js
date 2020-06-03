5313a4e85368ced390b4f4ba6069a651bb0caa8f
Andy Tran <transient.tran@gmail.com>
2020-06-03 10:24:43 -0400

    emit current players to everyone

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const players = [];
 io.on('connection', (socket) => {
     console.log(`A user connected: ${socket.id}`);
 
+    io.emit('currentPlayers', players);
+
     socket.on('newPlayer', (playerName, callback) => {
         if (_.find(players, { id: socket.id })) return;
         players.push({
@@ -15,6 +17,7 @@ io.on('connection', (socket) => {
             id: socket.id,
         });
         callback(playerName);
+        io.emit('currentPlayers', players);
     });
 
     socket.on('random', function () {
