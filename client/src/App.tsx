5c766793278cbb86ba57ca55b9c32915d2ab5479
Andy Tran <transient.tran@gmail.com>
2020-06-04 16:15:12 -0400

    move code into Board

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,21 @@
 import React from 'react';
-import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex } from '@trandromeda/react-hexgrid'
+
+import Details from './components/Details';
+import Board from './components/Board';
 
 import './App.scss';
-import Details from './components/Details';
 
 function App() {
-    const handleClick = (event: any, hex: any) => {
-        console.log(hex);
-    }
     return (
         <div className="app">
             <h1>Zodiac</h1>
             {/* <Canvas /> */}
-            <div className="app__details">
-                <Details />
-            </div>
 
             <div className="app__board">
-
-                <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
-                    {/* Grid with manually inserted hexagons */}
-                    <Layout size={{ x: 10, y: 10 }} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
-                        <Hexagon q={0} r={0} s={0} />
-                        {/* Using pattern (defined below) to fill the hexagon */}
-                        <Hexagon q={0} r={-1} s={1} onClick={handleClick} />
-                        <Hexagon q={0} r={1} s={-1} />
-                        <Hexagon q={1} r={-1} s={0}>
-                            <Text>1, -1, 0</Text>
-                        </Hexagon>
-                        <Hexagon q={1} r={0} s={-1}>
-                            <Text>1, 0, -1</Text>
-                        </Hexagon>
-                        {/* Pattern and text */}
-                        <Hexagon q={-1} r={1} s={0} onClick={handleClick} >
-                            <Text>-1, 1, 0</Text>
-                        </Hexagon>
-                        <Hexagon q={-1} r={0} s={1} />
-                        <Hexagon q={-2} r={0} s={1} />
-                        <Path start={new Hex(0, 0, 0)} end={new Hex(-2, 0, 1)} />
-                    </Layout>
-                </HexGrid>
-
+                <Board />
+            </div>
+            <div className="app__details">
+                <Details />
             </div>
         </div>
     );
