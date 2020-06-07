2dee270d7ebf62a4abbf2bb9fc3530f595a41043
Andy Tran <transient.tran@gmail.com>
2020-06-07 19:15:08 -0400

    connect Details to game stage context

--- client/src/components/details/Details.tsx
+++ client/src/components/details/Details.tsx
@@ -4,7 +4,7 @@ import './Details.scss';
 
 import PlayerName from './PlayerName';
 import Archetype from 'src/components/archetype/Archetype';
-import { GameStore } from 'src/store';
+import { GameStore } from 'src/game-store';
 import { IArchetype } from '../archetype/archetype.model';
 
 type Props = {
@@ -13,8 +13,11 @@ type Props = {
 
 function Details(props: Props) {
     const [players, setPlayers] = useState([{ playerName: '', id: '' }]);
-    const [gameState, gameDispatch] = useContext(GameStore);
-    console.log(gameState);
+    const { gameState, gameDispatch } = useContext(GameStore);
+
+    const handleEnterPreparationPhase = () => {
+        gameDispatch({ type: 'next-stage', payload: 'preparation' });
+    };
 
     useEffect(() => {
         socket.on('currentPlayers', (playerNames: { playerName: string; id: string }[]) => {
@@ -22,21 +25,29 @@ function Details(props: Props) {
         });
     }, []);
 
+    useEffect(() => console.log(gameState));
+
     return (
         <div className="details">
+            <h2>Phase: {gameState.stage}</h2>
             <div className="details__player-name">
                 <PlayerName />
             </div>
-            {players[0].playerName && (
-                <div className="details">
-                    <p>Players:</p>
-                    <ul>
-                        {players.map((player) => {
-                            return <li key={player.id}>{player.playerName}</li>;
-                        })}
-                    </ul>
-                </div>
-            )}
+            <div className="details__players-list">
+                {players[0].playerName && (
+                    <div className="details">
+                        <p>Players:</p>
+                        <ul>
+                            {players.map((player) => {
+                                return <li key={player.id}>{player.playerName}</li>;
+                            })}
+                        </ul>
+                    </div>
+                )}
+            </div>
+            <div className="details__prepare">
+                <button onClick={handleEnterPreparationPhase}>All players ready</button>
+            </div>
             <div>
                 {props.archetypes.map((archetype: IArchetype) => {
                     return <Archetype key={archetype.id} archetype={archetype} />;
