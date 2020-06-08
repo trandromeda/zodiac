8e35ed724064722bd8af293adc514e89cf755cf4
Andy Tran <transient.tran@gmail.com>
2020-06-07 23:30:04 -0400

    WIP working on archetype data

--- client/src/data/archetypes.ts
+++ client/src/data/archetypes.ts
@@ -3,20 +3,20 @@ export const archetypes: IArchetype[] = [
     {
         id: 'innocent',
         name: 'Innocent',
-        flavour: '',
+        flavour: 'A tremendous power dwells beneath a tranquil sea',
         imageUrl: './images/innocent.jpg',
         description: 'Render a memory untouchable (by players and abilities) for the next 2 turns.',
         charges: 2,
         turnType: 'instant',
     },
-    {
-        id: 'sage',
-        name: 'Sage',
-        flavour: '',
-        imageUrl: './images/sage.jpg',
-        description:
-            'Choose and point at any memory. The Vessel will indicate to you with a Yes or No whether that memory and all adjacent memories are safe.',
-        charges: 1,
-        turnType: 'instant',
-    },
+    // {
+    //     id: 'sage',
+    //     name: 'Sage',
+    //     flavour: '',
+    //     imageUrl: './images/sage.jpg',
+    //     description:
+    //         'Choose and point at any memory. The Vessel will indicate to you with a Yes or No whether that memory and all adjacent memories are safe.',
+    //     charges: 1,
+    //     turnType: 'instant',
+    // },
 ];
