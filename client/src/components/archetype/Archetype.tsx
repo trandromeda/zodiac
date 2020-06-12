56be39a9888f19aeda5b3d06bc63c915a17a3c66
Andy Tran <transient.tran@gmail.com>
2020-06-11 23:20:09 -0400

    handle archetype in component

--- client/src/components/archetype/Archetype.tsx
+++ client/src/components/archetype/Archetype.tsx
@@ -1,27 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import socket from 'src/utils/socket';
+import { find } from 'lodash';
+
+import { archetypesData } from 'src/data/archetypes.data';
+import ArchetypeClass from 'src/components/archetype/archetype.model';
 
 import './Archetype.scss';
-import { IArchetype } from 'src/components/archetype/archetype.model';
 
-interface Props {
-    archetype: IArchetype;
-}
+const Archetype = () => {
+    const [playerArchetype, setArchetype] = useState<ArchetypeClass | undefined>(undefined);
 
-const Archetype = ({ archetype }: Props) => {
-    const { id, name, description, flavour, charges, turnType } = archetype;
+    useEffect(() => {
+        socket.on('dealtArchetype', (archetypeId: string) => {
+            const archetypeObj = find(archetypesData, (arc) => arc.id === archetypeId);
+            if (archetypeObj) {
+                const archetype = new ArchetypeClass(archetypeObj);
+                setArchetype(archetype);
+            } else {
+                setArchetype(undefined);
+            }
+        });
+    }, []);
 
-    return (
-        <div className="archetype">
-            <p className="archetype__name">{name}</p>
-            <img src={require(`../../assets/${id}.jpg`)} />
-            <p className="archetype__description">{description}</p>
-            <div className="archetype__footer">
-                <p className="footer__charges">{charges}</p>
-                <p className="footer__turn">{turnType}</p>
-                <p className="footer__flavour">{flavour}</p>
+    if (playerArchetype) {
+        const { id, name, description, flavour, charges, turnType } = playerArchetype;
+        return (
+            <div className="archetype">
+                <p className="archetype__name">{name}</p>
+                <img src={require(`../../assets/${id}.jpg`)} />
+                <p className="archetype__description">{description}</p>
+                <div className="archetype__footer">
+                    <p className="footer__charges">{charges}</p>
+                    <p className="footer__turn">{turnType}</p>
+                    <p className="footer__flavour">{flavour}</p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else return <div></div>;
 };
 
 export default Archetype;
